c8f4e64c353c7e212ab06b020de0e82a96bd0fe1
vipulsingh <vipulspring@gmail.com>
2024-05-28 21:31:32 +0530

    first commit

---
+++ DatabaseProjectProgrammingSchool/dbo/Tables/Student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[Student] (
+    [StudentId] INT            NOT NULL,
+    [Name]      NVARCHAR (50)  NOT NULL,
+    [Detail]    NVARCHAR (150) NOT NULL,
+    PRIMARY KEY CLUSTERED ([StudentId] ASC)
+);
+
+
+GO
+
---
+++ DatabaseProjectProgrammingSchool/dbo/Tables/Teacher.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[Teacher]
+(
+  [Id] INT NOT NULL PRIMARY KEY
+)
